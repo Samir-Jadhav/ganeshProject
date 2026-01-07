b13074d63fcff19e3c3b694b3eb0ed80039f18a6
Samir Jadhav <jadhavsamir644@gmail.com>
2026-01-07 15:59:21 +0530

    Second commit

--- src/main/java/BaseClassPack/BaseClass.java
+++ src/main/java/BaseClassPack/BaseClass.java
@@ -5,8 +5,11 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -19,21 +22,42 @@ public class BaseClass {
 	
 	
 	void launchBrowser(String browser) throws IOException {
+		
 
 		
-		if(browser.equalsIgnoreCase("chrome")) {
-			
-			driver = new ChromeDriver();
-			
-		}else if(browser.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-			
-		}else if(browser.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
-		}else {
-			throw new IllegalArgumentException("Invalid browser: "+browser);
-		}
-	}
+		 boolean headless =
+			        Boolean.parseBoolean(ReadWriteProperty.readProperty("headless"));
+
+			    if (browser.equalsIgnoreCase("chrome")) {
+
+			        ChromeOptions options = new ChromeOptions();
+			        if (headless) {
+			            options.addArguments("--headless=new");
+			            options.addArguments("--window-size=1920,1080");
+			        }
+			        driver = new ChromeDriver(options);
+
+			    } else if (browser.equalsIgnoreCase("firefox")) {
+
+			        FirefoxOptions options = new FirefoxOptions();
+			        if (headless) {
+			            options.addArguments("-headless"); // CORRECT
+			        }
+			        driver = new FirefoxDriver(options);
+
+			    } else if (browser.equalsIgnoreCase("edge")) {
+
+			        EdgeOptions options = new EdgeOptions();
+			        if (headless) {
+			            options.addArguments("--headless=new");
+			            options.addArguments("--window-size=1920,1080");
+			        }
+			        driver = new EdgeDriver(options);
+
+			    } else {
+			        throw new IllegalArgumentException("Invalid browser: " + browser);
+			    }
+			}
 	
 	void configureBrowser() throws IOException {
 		
